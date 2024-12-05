b1833106bbe39c6ab69d432ba802402de4b71f8a
TimeoCHATELAIN <124506231+TimeoCHATELAIN@users.noreply.github.com>
2024-12-06 00:52:50 +0100

    jeu intronisé

---
+++ src/views/JeuView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <JeuComponent />
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<script setup>
+
+import JeuComponent from '@/components/jeu/JeuComponent.vue';
+
+</script>
