3acbc480ca2dc6dcaccf453b4074046220899ff6
nat-wd <nathan.roi@disroot.org>
2024-12-06 00:56:02 +0100

    installation de la lib pour les animations au scroll

--- src/components/credits/CreditsComponent.vue
+++ src/components/credits/CreditsComponent.vue
@@ -1,11 +1,48 @@
 <script setup>
+// Importation d'AOS
+  import { onMounted, onBeforeUnmount } from 'vue';
+  import AOS from 'aos';
+  import 'aos/dist/aos.css';
 
+  // Initialisation d'AOS lors du montage du composant
+  onMounted(() => {
+    AOS.init({
+      duration: 1200, // Durée de l'animation
+      offset: 200,    // Décalage avant que l'animation ne se déclenche
+    });
+  });
+
+  // Rafraîchissement d'AOS lorsqu'il y a une mise à jour dans le composant
+  onBeforeUnmount(() => {
+    AOS.refresh();
+  });
 </script>
 
 <template>
 
+  <div class="space"></div>
+  <div class="quare-1" data-aos="flip-up"></div>
+  <div class="space"></div>
+  <div class="quare-2" data-aos="fade-up"></div>
+  <div class="space"></div>
+  <div class="space"></div>
+  <div class="space"></div>
+
 </template>
 
 <style scoped>
+.space{
+  height: 100vh;
+}
+.quare-1{
+  height: 400px;
+  width: 500px;
+  background: red;
+}
 
+.quare-2{
+  height: 400px;
+  width: 500px;
+  background: blue;
+}
 </style>
--- src/views/CreditsView.vue
+++ src/views/CreditsView.vue
@@ -1,9 +1,11 @@
 <script setup>
-
+import CreditsComponent from '@/components/credits/CreditsComponent.vue'
 </script>
 
 <template>
-
+  <main>
+    <CreditsComponent />
+  </main>
 </template>
 
 <style scoped>
